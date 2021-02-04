870140aeaa7288f72f08a8e4d3b5f4d633ce9400
Yi Xiang Chen <yxiangch@everis.com>
2021-02-04 10:26:15 +0100

    Prod enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBhC-Q-S9OWaLdejHtHiSJBiiqQ-Yf9cfc",
+    authDomain: "angularfirebase-ba5f2.firebaseapp.com",
+    databaseURL: "https://angularfirebase-ba5f2-default-rtdb.firebaseio.com",
+    projectId: "angularfirebase-ba5f2",
+    storageBucket: "angularfirebase-ba5f2.appspot.com",
+    messagingSenderId: "602740426309",
+    appId: "1:602740426309:web:6b188d2a9f27b74702052c",
+    measurementId: "G-WP1KG6WQEZ"
+  },
 };
